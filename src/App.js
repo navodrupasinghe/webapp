e480368b55465190558006acfe8e64f117fa8a40
navodrupasinghe <navodrupasingha@gmail.com>
2023-04-01 23:46:34 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import "./App.css";
+//import Login from "./components/Login";
+import Register from "./components/Register";
+function App() {
+  return (
+    <div className="Register">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './Register.css';
+
+function Register() {
+  return (
+    <div className={styles.RegisterContainer}>
+      { /*Your sign up form goes here */}
+      <div className="box">
+          <h1>Login to Your Account</h1>
+    </div>
+    </div>
+  );
+}
+
+export default Register;
